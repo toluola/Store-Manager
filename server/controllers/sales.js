c3a37f93f4c6f8f272298b8e1156a26c738c4618
Tolu Olaniyan <toluola7@gmail.com>
2018-10-28 15:04:48 +0100

    [Chore #161531306] updating server-side

--- server/controllers/sales.js
+++ server/controllers/sales.js
@@ -1,75 +1,68 @@
 import _ from "underscore";
 
 const sales = [];
-
-export const getSales = (req, res) => {
-	if (sales.length > 0) {
-		res.status(200).json({
-			message: "Sales orders successfully fetched",
-			result: sales
-		});
-	} else {
-		res.status(404).json({
-			message: "No sales order found"
-		});
-	}
-};
-
-export const postSales = (req, res) => {
-	const sale = {
-		product_info: {
-			id: req.body.id,
-			name: req.body.name,
-			price: req.body.price,
-			quantity: req.body.quantity,
-			created_by: req.body.created_by
+class SalesController {
+	static getSales(req, res) {
+		if (sales.length > 0) {
+			res.status(200).json({
+				message: "Sales orders successfully fetched",
+				result: sales
+			});
+		} else {
+			res.status(404).json({
+				message: "No sales order found"
+			});
 		}
-	};
-	const idSave = req.body.id;
-	const nameSave = req.body.name;
-	const priceSave = req.body.price;
-	const quantitySave = req.body.quantity;
-	const createdbySave = req.body.quantity;
-	const newId = sales.filter(user => user.id === idSave)[0];
-	const newName = sales.filter(user => user.name === nameSave)[0];
-	if (newId || newName) {
-		res.status(400).json({
-			message: "Bad request"
-		});
-	} else if (
-		idSave === "" ||
-		nameSave === "" ||
-		quantitySave === "" ||
-		createdbySave === "" ||
-		priceSave === "" ||
-		typeof priceSave !== "number" ||
-		typeof quantitySave !== "number"
-	) {
-		res.status(404).json({
-			message: "Not found"
-		});
-	} else {
-		const added = _.each(sale, item => {
-			sales.push(item);
-		});
-		res.status(201).json({
-			message: "sales order added successfully",
-			createdProduct: sale
-		});
 	}
-};
 
-export const getEachSales = (req, res) => {
-	const id = req.params.Id;
-	const data = sales.filter(user => user.id === id)[0];
-	if (data) {
-		res.status(200).json({
-			message: "Sale order successfully fetched",
-			result: data
-		});
-	} else {
-		res.status(404).json({
-			message: "Sales record not found"
-		});
+	static postSales(req, res) {
+		const sale = {
+			product_info: {
+				id: req.body.id,
+				name: req.body.name,
+				price: req.body.price,
+				quantity: req.body.quantity,
+				created_by: req.body.created_by
+			}
+		};
+		const idSave = req.body.id;
+		const nameSave = req.body.name;
+		const newId = sales.filter(user => user.id === idSave)[0];
+		const newName = sales.filter(user => user.name === nameSave)[0];
+
+		if (newId) {
+			res.status(400).json({
+				message: `The Id '${idSave}' you Entered already exist`
+			});
+		} else if (newName) {
+			res.status(400).json({
+				message: `The Name '${nameSave}' you Entered already exist`
+			});
+		} else {
+			const added = _.each(sale, item => {
+				sales.push(item);
+			});
+			res.status(201).json({
+				message: "Sales record added successfully",
+				createdProduct: sale
+			});
+		}
 	}
-};
+
+	static getEachSales(req, res) {
+		const id = req.params.Id;
+		const data = sales.filter(user => user.id === id)[0];
+		if (data) {
+			res.status(200).json({
+				message: "Sale order successfully fetched",
+				result: data
+			});
+		} else {
+			res.status(404).json({
+				message: "Sales record not found"
+			});
+		}
+	}
+}
+
+export default SalesController;
